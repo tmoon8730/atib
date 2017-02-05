bf82eb22ec2186d90eca9379ae32b3f577de7ee4
alcamech <alcamech@gmail.com>
2017-02-05 10:25:10 -0500

    initalized a spring boot app for atib

---
+++ src/main/java/com/alcamech/atib/AtibApplication.java
@@ -0,0 +1,12 @@
+package com.alcamech.atib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtibApplication.class, args);
+	}
+}
