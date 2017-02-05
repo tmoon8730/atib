c728e98eda1a9d8012839fef415e63666cd37a63
Tyler Moon <moon.tyler@gmail.com>
2017-02-05 11:37:17 -0500

    added init project for atib-data

---
+++ atib-data/src/main/java/com/atib/AtibDataApplication.java
@@ -0,0 +1,12 @@
+package com.atib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtibDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtibDataApplication.class, args);
+	}
+}
---
+++ atib-data/src/test/java/com/atib/AtibDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtibDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
