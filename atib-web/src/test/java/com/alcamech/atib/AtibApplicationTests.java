28f888514563ae17b7523b7515064eaa54d003d3
Tyler Moon <moon.tyler@gmail.com>
2017-02-05 11:09:42 -0500

    forgot new directories

---
+++ atib-web/src/test/java/com/alcamech/atib/AtibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alcamech.atib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
