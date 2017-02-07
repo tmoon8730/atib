21fecef725c058298a101b71610611e3c8180c6e
Tyler Moon <moon.tyler@gmail.com>
2017-02-06 19:46:15 -0500

    Added in Twitter API stuff

--- atib-data/src/main/java/com/atib/AtibDataApplication.java
+++ atib-data/src/main/java/com/atib/AtibDataApplication.java
@@ -3,10 +3,13 @@ package com.atib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.atib.twitter.GetTimeline;
+
 @SpringBootApplication
 public class AtibDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtibDataApplication.class, args);
+		GetTimeline gt = new GetTimeline();
 	}
 }
